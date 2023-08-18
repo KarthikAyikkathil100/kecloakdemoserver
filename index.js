1594d92d5db59ab8179a5ff2403af64d8657c90b
Kartik Ayikkathil <kartik.ayikkathil@mindinventory.com>
2023-08-18 14:27:04 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import documents from "./Routes/documents.js";
+import authenticate from "./Routes/authenticate.js";
+
+(async function () {
+  dotenv.config();
+
+  const { PORT } = process.env;
+  const app = express();
+  // CORS permission
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    next();
+  });
+  const server = app.listen(PORT, () =>
+    console.log(`Backend started on port ${PORT}`)
+  );
+  app.get("/test", (req, res) => {
+    res.send("Server is runmning!!");
+  });
+
+  app.use("/documents", authenticate, documents);
+})();
